088a675b6435565e1816588d8b4bdf42ef64f670
Alaattin Ataç <alaattinatac@hotmail.com>
2021-12-25 00:53:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let myAge=prompt("What is your Age?");
+let earlyYears=2;
+let laterYears=(myAge-2);
+
+myAgeInDogYears= (earlyYears*10.5)+(laterYears*4);
+let myName= prompt("What is your name?");
+alert(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
